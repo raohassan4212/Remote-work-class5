b81e7fadcbcbb307926faead50e22c421d033e25
Rao Hassan <raohassan4212@gmail.com>
2020-10-22 13:29:38 +0500

    add Increment Reducer button

--- src/child2.js
+++ src/child2.js
@@ -4,11 +4,15 @@ import counterReducer from './counterReducer';
 const Child2 = () => {
 
     let [state, dispatch] = useReducer(counterReducer,10);
-    console.log(state);
+    
     return (
         <div>
             <br/>
             <h1>Child 2</h1>
+            <br/>
+            <p>Initial value of Reducer is: {state}</p>
+            <br/>
+            <button onClick={() => dispatch('INCREMENT')}>Increment Reducer</button>
         </div>
     );
 }
